096ec2f9d76b70dab567b75cae7550424d9c728e
jules-pierce <jupierce@seas.upenn.edu>
2021-06-24 17:32:23 -0600

    starter files

---
+++ README.md
@@ -0,0 +1,3 @@
+### CIS 573 Assignment 3 - Fault Tolerance
+
+Project writeup can be found here (TODO add link)
---
+++ ReliablePathFinder.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import edu.upenn.cis573.graphs.*;
+
+public class ReliablePathFinder extends PathFinder {
+	
+	public ReliablePathFinder(Graph g) {
+		super(g);
+	}
+
+	/*
+	 * Implement this method using a Recovery Block and Retry Block as described
+	 * in the assignment specification.
+	 */
+	public List<Integer> findPath(int src, int dest) throws PathNotFoundException {
+		
+		throw new PathNotFoundException();
+
+	}
+	
+	/*
+	 * Implement this acceptance test as described in the assignment specification.
+	 */
+	public boolean isValidPath(int src, int dest, List<Integer> path) {
+		
+		return false;
+
+	}
+
+
+}
