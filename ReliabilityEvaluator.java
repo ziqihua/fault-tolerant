a5bf681be7958c908e1579c53ef100cf0f138dbb
Ziqi Hua <ziqihua@seas.upenn.edu>
2024-06-19 18:24:37 -0400

    done

--- ReliabilityEvaluator.java
+++ ReliabilityEvaluator.java
@@ -7,47 +7,70 @@ public class ReliabilityEvaluator {
     private static Random rand = new Random(System.currentTimeMillis());
 
     public static void main(String[] args) {
-
-        // basically a Scanner
         In in = new In("graph.txt");
-
-        // construct a Graph from the input file
         System.out.println("Building the graph, please wait a moment...");
         Graph graph = new Graph(in);
         System.out.println("Done building the graph.");
 
         ReliablePathFinder finder = new ReliablePathFinder(graph);
 
-        // evaluate the reliability and execution time of ReliablePathFinder.findPath
-        // using 1000 trials
+        ReliablePathFinder.SearchType[][] orders = {
+                {
+                        ReliablePathFinder.SearchType.BFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.DFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.BFS_DEST_SRC,
+                        ReliablePathFinder.SearchType.DFS_DEST_SRC
+                },
+                {
+                        ReliablePathFinder.SearchType.DFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.BFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.DFS_DEST_SRC,
+                        ReliablePathFinder.SearchType.BFS_DEST_SRC
+                },
+                {
+                        ReliablePathFinder.SearchType.BFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.BFS_DEST_SRC,
+                        ReliablePathFinder.SearchType.DFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.DFS_DEST_SRC
+                },
+                {
+                        ReliablePathFinder.SearchType.DFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.DFS_DEST_SRC,
+                        ReliablePathFinder.SearchType.BFS_SRC_DEST,
+                        ReliablePathFinder.SearchType.BFS_DEST_SRC
+                }
+        };
+
+        for (int i = 0; i < orders.length; i++) {
+            evaluateOrder(finder, orders[i], i + 1);
+        }
+    }
+
+    private static void evaluateOrder(ReliablePathFinder finder, ReliablePathFinder.SearchType[] order, int orderNumber) {
         int success = 0, fail = 0;
-        System.out.println("Running the trials, please wait a moment...");
         long start = System.currentTimeMillis();
+
         for (int i = 0; i < 1000; i++) {
-            if (runTrial(finder)) {
+            if (runTrial(finder, order)) {
                 success++;
             } else {
                 fail++;
             }
         }
+
         long time = System.currentTimeMillis() - start;
-        System.out.println("Success: " + success + "; Fail: " + fail + "; Time: " + time + "ms");
+        System.out.println("Order " + orderNumber + " - Success: " + success + "; Fail: " + fail + "; Time: " + time + "ms");
     }
 
-    private static boolean runTrial(ReliablePathFinder finder) {
-        // randomly generate source and destination vertices
+    private static boolean runTrial(ReliablePathFinder finder, ReliablePathFinder.SearchType[] order) {
         int src = rand.nextInt(10000);
         int dest = rand.nextInt(10000);
 
         try {
-            finder.findPath(src, dest);
-            // if it returns, then it must have passed the acceptance test
+            finder.findPath(src, dest, order);
             return true;
         } catch (PathNotFoundException e) {
-            // if it throws an exception, it must have failed to find a path
             return false;
         }
-
     }
-
 }
--- ReliablePathFinder.java
+++ ReliablePathFinder.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 import edu.upenn.cis573.graphs.*;
 
@@ -8,23 +8,82 @@ public class ReliablePathFinder extends PathFinder {
         super(g);
     }
 
-    /*
-     * Implement this method using a Recovery Block and Retry Block as described in
-     * the assignment specification.
-     */
-    public List<Integer> findPath(int src, int dest) throws PathNotFoundException {
+    // Enum to define the type of search to perform
+    public enum SearchType {
+        BFS_SRC_DEST,
+        DFS_SRC_DEST,
+        BFS_DEST_SRC,
+        DFS_DEST_SRC
+    }
 
+    // Method to run a specific search type
+    private List<Integer> runSearch(SearchType type, int src, int dest) {
+        switch (type) {
+            case BFS_SRC_DEST:
+                return bfs(src, dest);
+            case DFS_SRC_DEST:
+                return dfs(src, dest);
+            case BFS_DEST_SRC:
+                return bfs(dest, src);
+            case DFS_DEST_SRC:
+                return dfs(dest, src);
+            default:
+                return null;
+        }
+    }
+
+    public List<Integer> findPath(int src, int dest, SearchType[] order) throws PathNotFoundException {
+        for (SearchType type : order) {
+            List<Integer> path = runSearch(type, src, dest);
+            if (type == SearchType.BFS_DEST_SRC || type == SearchType.DFS_DEST_SRC) {
+                Collections.reverse(path);
+            }
+            if (isValidPath(src, dest, path)) {
+                return path;
+            }
+        }
         throw new PathNotFoundException();
-
     }
 
-    /*
-     * Implement this acceptance test as described in the assignment specification.
-     */
+    // Overloaded findPath method to use the default ordering
+    @Override
+    public List<Integer> findPath(int src, int dest) throws PathNotFoundException {
+        SearchType[] defaultOrder = {
+                SearchType.BFS_SRC_DEST,
+                SearchType.DFS_SRC_DEST,
+                SearchType.BFS_DEST_SRC,
+                SearchType.DFS_DEST_SRC
+        };
+        return findPath(src, dest, defaultOrder);
+    }
+
+
     public boolean isValidPath(int src, int dest, List<Integer> path) {
+        if (path == null || path.isEmpty()) {
+            return false;
+        }
 
-        return false;
+        if (path.get(0) != src || path.get(path.size() - 1) != dest) {
+            return false;
+        }
 
+        for (int i = 0; i < path.size() - 1; i++) {
+            int current = path.get(i);
+            int next = path.get(i + 1);
+            boolean edgeExists = false;
+
+            for (int adjacent : graph.adj(current)) {
+                if (adjacent == next) {
+                    edgeExists = true;
+                    break;
+                }
+            }
+
+            if (!edgeExists) {
+                return false;
+            }
+        }
+
+        return true;
     }
-
 }
